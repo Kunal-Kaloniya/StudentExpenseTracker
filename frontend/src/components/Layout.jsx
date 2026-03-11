b86a9d33ea306eec63d02edaf8fd8c615969fa08
Kunal-Kaloniya <kunalk180305@gmail.com>
2026-03-11 16:39:50 +0530

    Create App Layout

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import { useAuthStore } from "../context/useAuthStore.js";
+import NavBar from "./NavBar.jsx";
+import Login from "./Login.jsx";
+
+function Layout() {
+    const currentUser = useAuthStore((state) => state.currentUser);
+
+    return (
+        <div className="min-h-screen flex bg-gray-50 font-sans text-gray-800">
+            {!currentUser && <Login />}
+
+            <NavBar />
+            <Outlet />
+        </div>
+    );
+}
+
+export default Layout;
