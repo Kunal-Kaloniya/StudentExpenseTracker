ac9f2aa63ba6dd24d424504ead0ac2684aaebd77
Kunal-Kaloniya <kunalk180305@gmail.com>
2026-03-14 22:30:54 +0530

    fix: cors options fixed as per cookies usage

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
 
 connectDB()
     .then(() => {
